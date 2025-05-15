4cb612c7b3551e7e413cab180a928a06cb9083e4
pablo <p.garcia-condeortiz-1@student.tudelft.nl>
2025-05-15 22:21:31 +0200

    sso repeat orbit file

---
+++ Astrodynamics/sso_repeat.py
@@ -0,0 +1,60 @@
+import numpy as np
+import pandas as pd
+
+def repeat_sso(days, tol=1e-6, max_iter=100, period_bounds_hr=(1.5, 3.0)):
+
+    # Constants
+    mu_mars = 42828.376645          # km^3 / s^2
+    R_M = 3396.2                    # equatorial radius, km
+    J2 = 0.001958705252674          # constant of oblateness, J2
+    P_sol = 24.6230*60*60           # solar rotation period, sec
+    n_sol = 1.059*10**(-7)          # mean rotation rate of Mars around the sun, rad/s
+
+    results = []
+
+    lower_Q = int(np.floor((days * P_sol) / (period_bounds_hr[1] * 3600)))
+    upper_Q = int(np.ceil((days * P_sol) / (period_bounds_hr[0] * 3600)))
+
+    a = R_M + 100                   # guess initial value of a
+
+    for Q in range(lower_Q, upper_Q + 1):
+
+        P_omega_true = days * P_sol / Q
+        a = R_M + 100               # Initial guess
+        converged = False
+
+        for _ in range(max_iter):
+            cos_i = -2 * n_sol * a**(7/2) / (3 * J2 * R_M**2 * np.sqrt(mu_mars))
+
+            if abs(cos_i) > 1:
+                break
+
+            i_rad = np.arccos(cos_i)
+
+            P_k = 2 * np.pi * np.sqrt(a**3 / mu_mars)
+
+            P_omega = P_k / (1 + 3 * R_M**2 * J2 * (3 - 4 * (np.sin(i_rad))**2) / (2 * a**2))
+
+            error = P_omega - P_omega_true
+
+            if abs(error) < tol and (a - R_M) > 0:
+                results.append({
+                    'Q': Q,
+                    'Inclination_deg': np.degrees(i_rad),
+                    'Semi_major_axis_km': a,
+                    'Altitude_km': a - R_M,
+                    'P_omega_hr': P_omega / 3600
+                })
+                
+                converged = True
+                
+                break
+
+            a *= (P_omega_true / P_omega)
+
+    df = pd.DataFrame(results)
+    print(df.to_string(index=False))
+        
+    return results
+
+repeat_sso(days=30)
