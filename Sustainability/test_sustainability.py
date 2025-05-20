ce3be359b66e4c01d2ab70b1824b1a24775401e8
pablo <p.garcia-condeortiz-1@student.tudelft.nl>
2025-05-20 15:11:00 +0200

    fix

--- Sustainability/test_sustainability.py
+++ Sustainability/test_sustainability.py
@@ -1,11 +1,9 @@
 import pytest
 import numpy as np
-from sustainability_tool import calculate_sustainability()
+from sustainability_tool import calculate_sustainability
 
 def test_sustainability_tool():
 
     sustainability_impact = calculate_sustainability()
 
     print(sustainability_impact)
-
-test_sustainability_tool()
