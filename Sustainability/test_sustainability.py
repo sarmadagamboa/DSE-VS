825f665aa0c99b5932c7512a48faace27b428060
pablo <p.garcia-condeortiz-1@student.tudelft.nl>
2025-05-20 15:08:45 +0200

    verification

---
+++ Sustainability/test_sustainability.py
@@ -0,0 +1,11 @@
+import pytest
+import numpy as np
+from sustainability_tool import calculate_sustainability()
+
+def test_sustainability_tool():
+
+    sustainability_impact = calculate_sustainability()
+
+    print(sustainability_impact)
+
+test_sustainability_tool()
