c452a56e159e3b56bace0cf1ec70ddafee9098fc
ViktorRaddum <viktorraddum@gmail.com>
2025-05-28 11:51:26 +0200

    Updating ADCS tools

--- Final Report/ADCS/SolarRadiationTorque.py
+++ Final Report/ADCS/SolarRadiationTorque.py
@@ -19,45 +19,34 @@ import matplotlib.pyplot as plt
 
 # Delta_x = (A_sc*x_sc + A_a*x_a)/A_s
 
+def maximum_torque(q, A_s, c_ps, c_g):
+    F_s = 1361 / 1.382**2  # Solar constant at Mars
+    c = 299792458  # Speed of light in m/s
+    i = 0  # Angle of incidence for maximum torque
+    F = F_s / c * A_s * (1 + q) * np.cos(i)
+    Tsp = F * (c_ps - c_g)
+    return Tsp
 
-# Solar constant
-F_s = 1361/1.382**2
+if __name__ == "__main__":
+    q = 0.6 # preliminary reflectance factor, a more comprehensive analysis is needed as it will vary across the spacecraft
+    # Area of the solar panels
+    A_sp = 4.5 * 2.9
 
-# Speed of light
-c = 299792458  # m/s
+    # Area of the side of the spacecraft
+    A_side = 4.5*1.45
 
-# Reflectance factor
-q = 0.6 # ask Bern about this
+    # Area of the antenna
+    A_a = 1.8**2 * np.pi  # Assuming a circular antenna with diameter 1.8 m
 
-# Area of the solar panels
-A_sp = 4.5 * 2.9
+    # Surface area
+    A_s = A_sp + A_side + A_a
 
-# Area of the side of the spacecraft
-A_side = 4.5*1.45
+    # Location of center of solar pressure
+    c_ps = (A_sp*2.9/2 + A_side*(2.9 + 1.45/2) + A_a*(2.9 + 1.45 + 1.8)) / A_s  # Weighted average position of the center of solar pressure, assuming the antenna is perpendicular to the sun
 
-# Area of the antenna
-A_a = 1.8**2 * np.pi  # Assuming a circular antenna with diameter 1.8 m
+    # Location of center of gravity
+    c_g = 2.9 + 1.45/2  # Assuming the center of gravity is at the center of the spacecraft side, measured from the bottom of the solar panels
 
-# Surface area
-A_s = A_sp + A_side + A_a
-
-# Angle of incidence
-i = np.linspace(-0.001, 0.001, 100) # radians, This is the maximum torque case
-
-# Location of center of solar pressure
-c_ps = (A_sp*2.9/2 + A_side*(2.9 + 1.45/2) + A_a*(2.9 + 1.45 + 1.8)) / A_s  # Weighted average position of the center of solar pressure, assuming the antenna is perpendicular to the sun
-
-# Location of center of gravity
-c_g = 2.9 + 1.45/2  # Assuming the center of gravity is at the center of the spacecraft side, measured from the bottom of the solar panels
-
-F = F_s/c*A_s*(1+q)*np.cos(i)
-Tsp = F*(c_ps - c_g)
-
-print(c_ps, c_g)
-
-plt.plot(i, Tsp)
-plt.xlabel('Angle of incidence (radians)')
-plt.ylabel('Solar Pressure Torque (Nm)')
-plt.title('Solar Pressure Torque vs Angle of Incidence')
-plt.grid()
-plt.show()
+    # Calculate the maximum torque
+    Tsp = maximum_torque(q, A_s, c_ps, c_g)
+    print(f"Maximum solar radiation torque: {Tsp} Nm, sign according to flight dynamics convention")
