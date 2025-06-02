9636499e7029c57c6a274c3a2f56fce9363bf86a
Pablo Garcia-Conde Ortiz <pgarciacondeor@tudelft.nl>
2025-06-02 14:22:51 +0200

    fix

--- Final Report/Payload/payloadpointing.py
+++ Final Report/Payload/payloadpointing.py
@@ -2,8 +2,8 @@ import numpy as np
 
 def calc_payload_pointing(baffle_diameter=0.025, mirror_distance=2):
 
-    req_angle = np.atan((baffle_diameter/2)/mirror_distance)
+    req_angle = np.arctan((baffle_diameter/2)/mirror_distance)
 
     return req_angle
 
-calc_payload_pointing()
+print(calc_payload_pointing())
