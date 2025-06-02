37f9f359a0a5acaa932dec983d9a685ea0535694
Pablo Garcia-Conde Ortiz <pgarciacondeor@tudelft.nl>
2025-06-02 22:35:56 +0200

    added ls 270 line

--- Final Report/Astrodynamics/marsdensity.py
+++ Final Report/Astrodynamics/marsdensity.py
@@ -3,7 +3,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 def plot_mars_density(year=2):
-
     if year == 2:
         data = np.loadtxt(r'Final Report\Astrodynamics\tpdhsy21.txt', skiprows=1) 
     elif year == 1:
@@ -17,7 +16,6 @@ def plot_mars_density(year=2):
 
     dmax_all = np.zeros_like(H, dtype=float)
     dmin_all = np.zeros_like(H, dtype=float)
-
     for i, h in enumerate(H):
         dens_at_h = density[height == h]
         if len(dens_at_h) > 0:
@@ -31,18 +29,27 @@ def plot_mars_density(year=2):
     dens_270 = density[mask_270]
     height_270 = height[mask_270]
 
-    d_270 = np.zeros_like(H, dtype=float)
+    dmax_270 = np.zeros_like(H, dtype=float)
+    dmin_270 = np.zeros_like(H, dtype=float)
     for i, h in enumerate(H):
         dens_at_h_270 = dens_270[height_270 == h]
         if len(dens_at_h_270) > 0:
-            d_270[i] = np.max(dens_at_h_270)
+            dmax_270[i] = np.max(dens_at_h_270)
+            dmin_270[i] = np.min(dens_at_h_270)
         else:
-            d_270[i] = np.nan
+            dmax_270[i] = np.nan
+            dmin_270[i] = np.nan
 
-    plt.figure(figsize=(8, 6))
-    plt.plot(np.log10(dmax_all), H, 'k', linewidth=3, label='Max density (all Ls)')
-    plt.plot(np.log10(dmin_all), H, 'b', linewidth=3, label='Min density (all Ls)')
-    plt.plot(np.log10(d_270), H, 'r--', linewidth=3, label='Max density (Ls=270°)')
+    plt.figure(figsize=(12, 6))
+
+    plt.scatter(np.log10(density), height, color='r', s=5, label='Density range (all Ls)')
+
+    plt.plot(np.log10(dmax_all), H, 'k-', linewidth=3, label='Max density (all Ls)')
+    plt.plot(np.log10(dmin_all), H, 'b-', linewidth=3, label='Min density (all Ls)')
+
+    plt.plot(np.log10(dmax_270), H, 'k--', linewidth=3, label='Max density (Ls=270°)')
+    plt.plot(np.log10(dmin_270), H, 'b--', linewidth=3, label='Min density (Ls=270°)')
+
     plt.xlabel('density (log10 kg/m³)')
     plt.ylabel('height (km)')
     if year == 2:
